c6d23f96f7efb222540af1d3a4041d30decc2b6d
Madelyn Puentes <contactmpuentes@gmail.com>
2026-08-15 19:23:43 -0500

    Add info icon + hover tooltip to Saved Documents header on Documents tab

--- app-bootstrap.js
+++ app-bootstrap.js
@@ -1,7 +1,7 @@
 import { portalConfig } from './config.js';
 
 const config = portalConfig || {};
-const APP_RUNTIME_MODULE = './app.js?v=20260815-15';
+const APP_RUNTIME_MODULE = './app.js?v=20260815-16';
 
 const state = {
   supabase: null,
