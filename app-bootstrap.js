26ca0bea921e021ed583ac719ffa33fd934b025d
Madelyn Puentes <contactmpuentes@gmail.com>
2026-08-28 15:07:26 -0500

    Fix sidebar collapse: hardcoded .shell grid rules now use --sidebar-w + explicit collapsed override

--- app-bootstrap.js
+++ app-bootstrap.js
@@ -1,7 +1,7 @@
 import { portalConfig } from './config.js';
 
 const config = portalConfig || {};
-const APP_RUNTIME_MODULE = './app.js?v=20260828-47';
+const APP_RUNTIME_MODULE = './app.js?v=20260828-48';
 
 const state = {
   supabase: null,
