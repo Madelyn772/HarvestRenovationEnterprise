cbe29f56d6f2216a9083c9d25b1296df529228c7
Madelyn Puentes <contactmpuentes@gmail.com>
2026-08-29 19:57:51 -0500

    Redesign contract PDF as Texas service agreement

--- app-bootstrap.js
+++ app-bootstrap.js
@@ -1,7 +1,7 @@
 import { portalConfig } from './config.js';
 
 const config = portalConfig || {};
-const APP_RUNTIME_MODULE = './app.js?v=20260829-103';
+const APP_RUNTIME_MODULE = './app.js?v=20260829-104';
 
 const state = {
   supabase: null,
