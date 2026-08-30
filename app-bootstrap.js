8fed891e5e3fa6af69921898de80746f82e87bb1
Madelyn Puentes <contactmpuentes@gmail.com>
2026-08-29 19:30:55 -0500

    Align collapsed sidebar controls

--- app-bootstrap.js
+++ app-bootstrap.js
@@ -1,7 +1,7 @@
 import { portalConfig } from './config.js';
 
 const config = portalConfig || {};
-const APP_RUNTIME_MODULE = './app.js?v=20260829-99';
+const APP_RUNTIME_MODULE = './app.js?v=20260829-100';
 
 const state = {
   supabase: null,
